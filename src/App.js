d55861cbeca23a0c231f19e7cc6396cb4b0e7887
deepakrana123 <rdev69146@gmail.com>
2023-01-10 18:40:57 +0530

    section-issue-resloved

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import Home from "./components/Home";
-import Section from "./components/Section"
 import Header from "./components/Header";
 function App() {
   return (
